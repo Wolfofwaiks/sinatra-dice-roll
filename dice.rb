5e3cebf93df731c27713aa1236cc4e0c923856e1
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-04-18 21:33:26 +0000

    lots of work!

--- dice.rb
+++ dice.rb
@@ -6,7 +6,7 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/diceroll") do
+get("/") do
   erb(:elephant)
 end
 
@@ -15,28 +15,26 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 get("/dice/2/10")  do 
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
-end 
-get("/dice/1/20")  do 
-  first_die = rand(1..20)
-  sum = first_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 
-  outcome = "You rolled a #{first_die} for a total of #{sum}"
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  erb(:two_ten)
 end 
+get("/dice/1/20") do
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
+end
 get("/dice/5/4")  do 
   first_die = rand(1..4)
   second_die = rand(1..4)
@@ -45,7 +43,6 @@ get("/dice/5/4")  do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
+ erb(:five_four)
 end 
