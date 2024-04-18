6b94e941f8e36c7a5f38a5ca4d7e2e4efce31e4f
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-04-18 15:52:28 +0000

    elelphant erb

--- dice.rb
+++ dice.rb
@@ -6,16 +6,10 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-
 get("/diceroll") do
-  "<h1>Dice Roll</h1>
-  <ul>
-    <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
-    <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
-    <li><a href='/dice/1/20'>Roll one 20-sided dice</a></li>
-    <li><a href='/dice/5/4'>Roll five 4-sided dice</a></li>
-  </ul>"
+  erb(:elephant)
 end
+
 get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
